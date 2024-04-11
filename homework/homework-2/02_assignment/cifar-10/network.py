c87048db235920247ac761ad7a62572dc598f434
EmptyBlue <lyt0112@outlook.com>
2024-04-12 04:15:25 +0800

    update: homework 2 complete
    Former-commit-id: 3186c43cd5edbec5d1055fdd36bec88f41dc51b5

---
+++ homework/homework-2/02_assignment/cifar-10/network.py
@@ -0,0 +1,76 @@
+import torch.nn as nn
+
+
+class ConvNet(nn.Module):
+    def __init__(self, num_classes=10):
+        super(ConvNet, self).__init__()
+        # VGG11
+        self.features = nn.Sequential(
+            # 第一个卷积块
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            # 第二个卷积块
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            # 第三个卷积块
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            # 第四个卷积块
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            # 第五个卷积块
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        # 调整全连接层的输入大小
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(512, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)  # 展平特征图
+        x = self.classifier(x)
+        return x
+
+
+if __name__ == '__main__':
+    import torch
+    from torch.utils.tensorboard import SummaryWriter
+    from dataset import CIFAR10
+    writer = SummaryWriter(log_dir='../experiments/network_structure')
+    net = ConvNet()
+    train_dataset = CIFAR10()
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=2, shuffle=False, num_workers=2)
+    # Write a CNN graph.
+    # Please save a figure/screenshot to '../results' for submission.
+    for imgs, labels in train_loader:
+        writer.add_graph(net, imgs)
+        writer.close()
+        break
---
+++ homework/homework-2/02_assignment/pack.py
@@ -0,0 +1,37 @@
+import os
+import zipfile
+
+
+def zipHW2(input_path: str, output_path: str):
+    zip = zipfile.ZipFile(output_path, "w", zipfile.ZIP_DEFLATED)
+    for path, dirnames, filenames in os.walk(os.path.join(input_path, "batch_normalization")):
+        fpath = path.replace(input_path, 'HW2')
+        for filename in filenames:
+            if filename in ["bn.py"]:
+                zip.write(os.path.join(path, filename), os.path.join(fpath, filename))
+    for path, dirnames, filenames in os.walk(os.path.join(input_path, "cifar-10")):
+        fpath = path.replace(input_path, 'HW2')
+        for filename in filenames:
+            if filename in ["dataset.py", "network.py", "train.py"]:
+                zip.write(os.path.join(path, filename), os.path.join(fpath, filename))
+    for path, dirnames, filenames in os.walk(os.path.join(input_path, "results")):
+        fpath = path.replace(input_path, 'HW2')
+        for filename in filenames:
+            zip.write(os.path.join(path, filename), os.path.join(fpath, filename))
+    zip.write(os.path.join(input_path, "HW1_BP.py"), os.path.join(input_path.replace(input_path, 'HW2'), "HW1_BP.py"))
+    zip.close()
+
+
+if __name__ == "__main__":
+
+    # ---------------------------------------------------------
+    # 请用你的学号和姓名替换下面的内容，注意参照例子的格式，使用拼音而非中文
+    id = 2100013116
+    name = 'LiangYutong'
+    # ---------------------------------------------------------
+
+    zip_name = f'{id}_{name}.zip'
+    input_path = os.path.dirname(__file__)
+    output_path = os.path.join(os.path.dirname(__file__), zip_name)
+
+    zipHW2(input_path, output_path)
